bb269b7f24aa310e4203bae77dc30462f5a39eec
Tim Miller <tim.miller@innogames.com>
2014-08-17 11:47:10 +0200

    fixed fear

--- htdocs/js/proto/entities/characters/Player.js
+++ htdocs/js/proto/entities/characters/Player.js
@@ -1095,8 +1095,18 @@ define([
 			dom.removeClass(elem2, 'hit');
 		}, 180);
 
-		if (window.fear == 3/*game.player.fear && game.inactivePlayer.fear && game.inactivePlayer2.fear*/) {
+
+
+		window['fear' + name] = true;
+		elem = dom.get("fear " + name);
+
+		dom.addClass(elem, "visible");
+
+
+		console.log('drin');
+		if (window.fearolaf && window.fearjerome && window.fearlina/*game.player.fear && game.inactivePlayer.fear && game.inactivePlayer2.fear*/) {
 			config.running = false;
+			console.log('dead');
 
 			// GAMEOVER
 			dom.addClass(dom.get("gameover"), "display");
@@ -1105,13 +1115,8 @@ define([
 
 			window.setTimeout(function() {
 				window.location.reload();
-			}, 4000);
+			}, 7000);
 		}
-
-		window['fear' + name] = true;
-		elem = dom.get("fear " + name);
-
-		dom.addClass(elem, "visible");
 
 		window.game.scene.add(window["cry" + name] = new Cry(name));
 	};
